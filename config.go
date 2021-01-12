71883d8b9fdcf3e029fbe9d98da36e6ec7553fa1
jamie1192 <jastley92@gmail.com>
2021-01-12 23:26:53 +1100

    [#5] allow setting custom repo url input for SSH apps

--- config.go
+++ config.go
@@ -7,9 +7,11 @@ type config struct {
 	AppTitle    string `env:"BITRISE_APP_TITLE"`
 	AppURL      string `env:"BITRISE_APP_URL"`
 	BuildURL    string `env:"BITRISE_BUILD_URL"`
-	RepoURL     string `env:"GIT_REPOSITORY_URL"`
 	GitBranch   string `env:"BITRISE_GIT_BRANCH"`
 	AppImageURL string `env:"BITRISE_APP_SLUG"`
 
 	WebhookURL string `env:"webhook_url,required"`
+
+	// Optional user inputs
+	RepoURL string `env:"repository_url"`
 }
