d175694cdb72681bcb922b69f95cba8a55c53341
jamie1192 <jastley92@gmail.com>
2021-01-12 23:05:01 +1100

    [#3] Added additional user inputs - optional colour options - custom card text - default values if none supplied

--- config.go
+++ config.go
@@ -3,13 +3,29 @@ package main
 // config will be populated with the retrieved values from environment variables
 // configured as step inputs.
 type config struct {
-	BuildNumber string `env:"BITRISE_BUILD_NUMBER"`
-	AppTitle    string `env:"BITRISE_APP_TITLE"`
-	AppURL      string `env:"BITRISE_APP_URL"`
-	BuildURL    string `env:"BITRISE_BUILD_URL"`
-	RepoURL     string `env:"GIT_REPOSITORY_URL"`
-	GitBranch   string `env:"BITRISE_GIT_BRANCH"`
-	AppImageURL string `env:"BITRISE_APP_SLUG"`
+	// Bitrise environment inputs
+	BuildNumber       string `env:"BITRISE_BUILD_NUMBER"`
+	AppTitle          string `env:"BITRISE_APP_TITLE"`
+	AppURL            string `env:"BITRISE_APP_URL"`
+	BuildURL          string `env:"BITRISE_BUILD_URL"`
+	BuildTime         string `env:"BITRISE_BUILD_TRIGGER_TIMESTAMP"`
+	CommitMessageBody string `env:"GIT_CLONE_COMMIT_MESSAGE_BODY"`
+	RepoURL           string `env:"GIT_REPOSITORY_URL"`
+	GitBranch         string `env:"BITRISE_GIT_BRANCH"`
+	AppImageURL       string `env:"BITRISE_APP_SLUG"`
+	Workflow          string `env:"BITRISE_TRIGGERED_WORKFLOW_TITLE"`
 
+	// Required user inputs
 	WebhookURL string `env:"webhook_url,required"`
+
+	// Optional user inputs
+	CardTitle          string `env:"card_title"`
+	SuccessThemeColor  string `env:"success_theme_color"`
+	FailedThemeColor   string `env:"failed_theme_color"`
+	SectionTitle       string `env:"section_title"`
+	SectionSubtitle    string `env:"section_subtitle"`
+	SectionText        string `env:"section_text"`
+	SectionHeaderImage string `env:"section_header_image"`
+	EnableMarkdown     bool   `env:"enable_markdown"`
+	EnableDebug        bool   `env:"enable_debug"`
 }
