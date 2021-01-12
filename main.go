3ce576b0d224c747e263bcb898a0454c03004a9b
jamie1192 <jastley92@gmail.com>
2021-01-12 23:07:57 +1100

    [#3] Implement custom user inputs - helper function to delegate content per build status - helper function to delegate default/provided user inputs - debug JSON body logging when enabled by user

--- main.go
+++ main.go
@@ -7,43 +7,71 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/bitrise-tools/go-steputils/stepconf"
 )
 
-func newMessage(buildOk bool, cfg config) Message {
+var buildSucceeded = os.Getenv("BITRISE_BUILD_STATUS") == "0"
+
+func buildStatusValue(ifSuccess, ifFailed string) string {
+	if buildSucceeded || ifFailed == "" {
+		return ifSuccess
+	}
+	return ifFailed
+}
+
+func optionalUserValue(defaultValue, userValue string) string {
+	if userValue == "" {
+		return defaultValue
+	}
+	return userValue
+}
+
+func newMessage(cfg config) Message {
 	message := Message{}
 	message.Type = "MessageCard"
 	message.Context = "http://schema.org/extensions"
-	message.ThemeColor = "0076D7"
-	if buildOk {
-		message.Summary = fmt.Sprintf("%s build number %s succeeded", cfg.AppTitle, cfg.BuildNumber)
-	} else {
-		message.Summary = fmt.Sprintf("%s build number %s failed", cfg.AppTitle, cfg.BuildNumber)
+	message.ThemeColor = buildStatusValue(
+		cfg.SuccessThemeColor,
+		cfg.FailedThemeColor,
+	)
+	message.Title = optionalUserValue(cfg.AppTitle, cfg.CardTitle)
+	message.Summary = buildStatusValue(
+		fmt.Sprintf("%s build #%s succeeded", cfg.AppTitle, cfg.BuildNumber),
+		fmt.Sprintf("%s build #%s failed", cfg.AppTitle, cfg.BuildNumber),
+	)
+	message.Markdown = strconv.FormatBool(cfg.EnableMarkdown)
+
+	buildStatusFact := Fact{
+		Name: "Build Status",
+		Value: buildStatusValue(
+			fmt.Sprintf(`<span style="color:#%s">Success</span>`, cfg.SuccessThemeColor),
+			fmt.Sprintf(`<span style="color:#%s">Fail</span>`, cfg.FailedThemeColor),
+		),
 	}
-	message.Markdown = "true"
-	section := Section{}
-	section.AppTitle = fmt.Sprintf("![AppImage](%s)%s", cfg.AppImageURL, cfg.AppTitle)
-	section.BuildNumber = cfg.BuildNumber
-	section.AppImage = cfg.AppImageURL
 
-	buildStatusFact := Fact{}
-	buildStatusFact.Name = "Build Status"
-	if buildOk {
-		buildStatusFact.Value = `<span style="color:green">Success</span>`
-	} else {
-		buildStatusFact.Value = `<span style="color:red">Fail</span>`
+	buildNumberFact := Fact{
+		Name:  "Build Number",
+		Value: cfg.BuildNumber,
 	}
 
-	buildNumberFact := Fact{}
-	buildNumberFact.Name = "Build Number"
-	buildNumberFact.Value = cfg.BuildNumber
+	buildBranchFact := Fact{
+		Name:  "Git Branch",
+		Value: cfg.GitBranch,
+	}
 
-	buildBranchFact := Fact{}
-	buildBranchFact.Name = "Git Branch"
-	buildBranchFact.Value = cfg.GitBranch
+	buildTimeFact := Fact{
+		Name:  "Build Triggered",
+		Value: cfg.BuildTime,
+	}
 
-	section.Facts = []Fact{buildStatusFact, buildNumberFact, buildBranchFact}
+	workflowFact := Fact{
+		Name:  "Workflow",
+		Value: cfg.Workflow,
+	}
+
+	section := buildSection(cfg, []Fact{buildStatusFact, buildNumberFact, buildBranchFact, buildTimeFact, workflowFact})
 
 	message.Sections = []Section{section}
 
@@ -53,6 +81,16 @@ func newMessage(buildOk bool, cfg config) Message {
 	message.Actions = []OpenUriAction{goToRepoAction, goToBuildAction}
 
 	return message
+}
+
+func buildSection(cfg config, facts []Fact) Section {
+	section := Section{}
+	section.ActivityTitle = cfg.SectionTitle
+	section.ActivitySubtitle = cfg.SectionSubtitle
+	section.Text = cfg.SectionText
+	section.ActivityImage = cfg.AppImageURL
+	section.Facts = facts
+	return section
 }
 
 func buildUriButton(buttonText string, uri string) OpenUriAction {
@@ -68,12 +106,15 @@ func buildUriButton(buttonText string, uri string) OpenUriAction {
 }
 
 // postMessage sends a message to a channel.
-func postMessage(webhookURL string, msg Message) error {
+func postMessage(webhookURL string, msg Message, debugEnabled bool) error {
 	b, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
 	fmt.Println(fmt.Sprintf("Request to Microsoft Teams: %s", webhookURL))
+	if debugEnabled {
+		fmt.Println(fmt.Sprintf("JSON body: %s", b))
+	}
 
 	resp, err := http.Post(webhookURL, "application/json", bytes.NewReader(b))
 	if err != nil {
@@ -104,10 +145,8 @@ func main() {
 	}
 	stepconf.Print(cfg)
 
-	// success is true if the build is successful, false otherwise.
-	success := os.Getenv("BITRISE_BUILD_STATUS") == "0"
-	message := newMessage(success, cfg)
-	if err := postMessage(cfg.WebhookURL, message); err != nil {
+	message := newMessage(cfg)
+	if err := postMessage(cfg.WebhookURL, message, cfg.EnableDebug); err != nil {
 		fmt.Println(fmt.Sprintf("Error: %s", err))
 		os.Exit(1)
 	}
